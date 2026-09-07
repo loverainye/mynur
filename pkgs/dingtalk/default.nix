8e9cecd1e4d0d9e082821e81f1fae71b077a9aa9
loverainye <loverainye@gmail.com>
2026-09-07 12:32:46 +0800

    update dingtalk

--- pkgs/dingtalk/default.nix
+++ pkgs/dingtalk/default.nix
@@ -161,11 +161,11 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "dingtalk";
-  version = "com.alibabainc.dingtalk_8.2.8.260812001";
+  version = "com.alibabainc.dingtalk_8.2.8.260904001";
 
   src = fetchurl {
-    url = "https://dtapp-pub.dingtalk.com/dingtalk-desktop/xc_dingtalk_update/linux_deb/Release/com.alibabainc.dingtalk_8.2.8.260812001_amd64.deb";
-    hash = "sha256-eNwy2JSt5D6sD2Jys3+rlFi/mWstAyhaQc0UFpfWZzA=";
+    url = "https://dtapp-pub.dingtalk.com/dingtalk-desktop/xc_dingtalk_update/linux_deb/Release/com.alibabainc.dingtalk_8.2.8.260904001_amd64.deb";
+    hash = "sha256-XMuDn54wSUmD+oqHwC0DtuaRXRMX9TfhfQjNiXrHULA=";
   };
 
   nativeBuildInputs = [
